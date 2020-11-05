d3f8d5c423ce30d9af42d182095a4404ce69dfee
MambusDev <marco.t07@hotmail.de>
2020-11-05 22:14:32 +0100

    Add view settings to example widget

--- apps/_example_widget/widget.js
+++ apps/_example_widget/widget.js
@@ -1,15 +1,42 @@
 /* run widgets in their own function scope so they don't interfere with
 currently-running apps */
 (() => {
+  const s = require('Storage');
+  const SETTINGS_FILE = 'widget.settings.json';
+  let settings;
+
   function draw() {
-    g.reset(); // reset the graphics context to defaults (color/font/etc)
+    // Draw only, if user wants the widget to be drawn
+    if (setting('showWidget') == false) {
+      return;
+    }
+
+    // reset the graphics context to defaults (color/font/etc)
+    g.reset();
+    
     // add your code
     g.drawString("X", this.x, this.y);
   }
 
+  //load settings
+  function loadSettings() {
+    settings = s.readJSON(SETTINGS_FILE, 1) || {};
+  }
+
+  //return setting
+  function setting(key) {
+    //define default settings
+    const DEFAULTS = {
+      'showWidget' : false,
+      'widgetArea' : "tl",
+    };
+    if (!settings) { loadSettings(); }
+    return (key in settings) ? settings[key] : DEFAULTS[key];
+  }
+
   // add your widget
   WIDGETS["mywidget"]={
-    area:"tl", // tl (top left), tr (top right), bl (bottom left), br (bottom right)
+    area:setting('widgetArea'), // area as selected in settings
     width: 28, // how wide is the widget? You can change this and call Bangle.drawWidgets() to re-layout
     draw:draw // called to draw the widget
   };
