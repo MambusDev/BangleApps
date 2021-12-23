168a25a8eae531520c42671e8bafc23deb9417e1
MambusDev <marco.t07@hotmail.de>
2021-12-23 01:31:15 +0100

    Change font and fix size calculations

--- apps/clistyle/clistyle.js
+++ apps/clistyle/clistyle.js
@@ -9,23 +9,50 @@ const textColor = {
   black: 5
 };
 
-const cliFont="6x8";
-const cliFontWidth=6;
-const cliFontHeight=8;
-const cliLineStart = 4;
+
+// Supported Fonts
+//require("FontTeletext10x18Ascii").add(Graphics);
+//require("FontHaxorNarrow7x17").add(Graphics);
+require("FontDylex7x13").add(Graphics);
+
+//const teletext10x18ascii = {name:"Teletext10x18Ascii", width:10, height:18};
+//const builtin4x6 = {name:"4x6", width:4, height:6};
+//const builtin6x8 = {name:"6x8", width:6, height:8};
+//const haxornarrow7x17 = {name:"HaxorNarrow7x17", width:7, height:17};
+const dylex7x13 = {name:"Dylex7x13", width:7, height:13};
+
+// Set font here:
+const cliFont = dylex7x13;
+
 const cliHeight = g.getHeight() - 2 * margin;
 const cliWidth = g.getWidth();
 
-var cliFontSize = 2;
+
+// Default font size is 1
+var cliFontSize = 1;
 var cursorFlag = false;
+
+// Cache cursor state
 var cursor = {
   initialized: false,
-  line: 0,
-  column: 0,
+  x: 0,
+  y: 0,
   show: false
 };
 
 // Private functions
+function cliLineHeight() {
+ return 1.25 * cliFont.height * cliFontSize; 
+}
+
+function cliColumnWidth() {
+  return (cliFont.width) * cliFontSize;
+}
+
+function cliLineStart() {
+  // Factor 1.5 to have some space between '>' and text
+  return 1.5 * cliColumnWidth();
+}
 
 function setTextColorRgb(color) {
   g.setBgColor(0,0,0);
@@ -52,51 +79,59 @@ function setTextColorRgb(color) {
 }
 
 function writeLineStart(line){
-  let lineHeight = cliFontHeight + cliFontHeight / 2;
   setTextColorRgb(textColor.green);
-  g.setFont(cliFont, cliFontSize);
+  g.setFont(cliFont.name, cliFontSize);
   g.setFontAlign(-1,-1);
-  g.drawString(">", cliLineStart, margin + line * cliFontSize * lineHeight);
+  g.drawString(">", cliLineStart() / 4, margin + line * cliLineHeight());
 }
 
 function clearLine(line) {
-  let lineHeight = cliFontHeight + cliFontHeight / 2;
   let x1 = 0;
-  let y1 = margin + line * cliFontSize * lineHeight - 2;
+  let y1 = margin + line * cliLineHeight();
   let x2 = cliWidth;
-  let y2 = y1 + cliFontSize * cliFontHeight + 2;
+  let y2 = y1 + cliLineHeight();
   g.setColor(0,0,0);
   g.fillRect(x1,y1,x2,y2);  
 }
 
 function writeLine(str, line, color){
-  let lineHeight = cliFontHeight + cliFontHeight / 2;
-  g.setFont(cliFont,cliFontSize);
+  g.setFont(cliFont.name, cliFontSize);
   g.setFontAlign(-1,-1);
   if (color == textColor.highlighted) {
     //draw green / white rectangle
     g.setColor(1,1,1);
-    let x1 = (2 * cliFontWidth * cliFontSize) - 2;
-    let y1 = margin + line * cliFontSize * lineHeight -2;
-    let x2 = x1 + str.length * cliFontSize * cliFontWidth;
-    let y2 = y1 + cliFontSize * cliFontHeight + 2;
+    let x1 = cliLineStart();
+    let y1 = margin + line * cliLineHeight();
+    let x2 = x1 + str.length * cliColumnWidth();
+    let y2 = y1 + cliLineHeight();
     g.fillRect(x1,y1,x2,y2); 
   }
   setTextColorRgb(color);
-  g.drawString(str, 1.5 * cliFontWidth * cliFontSize, margin + line * cliFontSize * lineHeight);
+  g.drawString(str, cliLineStart(), margin + line * cliLineHeight());
+}
+
+function drawCursor() {
+  let lineWidth = 1;
+  
+  let x1 = cursor.x;
+  let x2 = x1 + lineWidth; 
+  let y1 = cursor.y;
+  let y2 = y1 + cliFont.height * cliFontSize;
+  
+  g.fillRect(x1, y2, x2, y1);
 }
 
 function cursorAnimation() {
   if (cursor.initialized){
-    let lineHeight = cliFontHeight + cliFontHeight / 2;
     setTextColorRgb(textColor.green);
     g.setFontAlign(-1,-1);
-    writeLineStart(cursor.line);
+    //writeLineStart(cursor.line);
+    
     if (cursor.show) {
-      g.drawString("|", (1.25 + cursor.column) * cliFontWidth * cliFontSize, margin + cursor.line * cliFontSize * lineHeight);
+      drawCursor();
     } else {
       setTextColorRgb(textColor.black);
-      g.drawString("|", (1.25 + cursor.column) * cliFontWidth * cliFontSize, margin + cursor.line * cliFontSize * lineHeight);
+      drawCursor();
     }
 
     cursor.show = !cursor.show;
@@ -109,8 +144,7 @@ function setFontSize(size) {
 }
 
 function clear(clear_widgets) {
-  let lineHeight = cliFontHeight + cliFontHeight / 2;
-  let lines = cliHeight / lineHeight;
+  let lines = cliHeight / cliLineHeight();
   
   if (clear_widgets) {
     g.clear();
@@ -120,7 +154,7 @@ function clear(clear_widgets) {
     }
   }
   
-  cursor = {initialized: false, line: 0, column: 0, show: false};
+  cursor = {initialized: false, x: 0, y: 0, show: false};
 }
 
 function printLine(str, color, line, update_cursor) {
@@ -128,8 +162,8 @@ function printLine(str, color, line, update_cursor) {
   writeLineStart(line);
   writeLine(str, line, color);
   if (update_cursor) {
-    cursor.line = line;
-    cursor.column = str.length;
+    cursor.x = g.stringWidth(str) + cliLineStart() + 0.25 * cliColumnWidth();
+    cursor.y = margin + line * cliLineHeight();
     cursor.initialized = true;
   }
 }
@@ -149,3 +183,10 @@ exports.printLine = function(str, color, line, update_cursor) {
 exports.textColor = textColor;
 
 var animationInterval = setInterval(cursorAnimation, 500);
+
+// Unit test
+//clear(true);
+//setFontSize(2);
+//printLine("12:33 Uhr", textColor.green, 1, false);
+//printLine("00p00", textColor.green, 2, true);
+//printLine(cursor.x, textColor.green, 3, false);
